e8121605dd51e0bca14f2e84f57c00c524383e86
YeiShari <lleuxmerveivp@gmail.com>
2025-10-06 15:41:20 -0500

    feat(profile): add account info section, language section, profile header, scenes section and toggle item components.

---
+++ src/profiles/presentation/components/language-section.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="language-section">
+    <pv-float-label>
+      <pv-input-text :modelValue="language" readonly />
+      <label>{{ $t('profile.sectionAppLanguage') }}</label>
+    </pv-float-label>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  language: String,
+});
+</script>
+
+<style scoped>
+.language-section { width: 100%; }
+</style>
