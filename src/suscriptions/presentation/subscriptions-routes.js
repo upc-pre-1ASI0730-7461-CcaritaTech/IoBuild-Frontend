f006cd0ecfdfab26d54f096177915d1d00d717a8
nOOmz <n00mr.axl@gmail.com>
2025-10-08 03:23:16 -0500

    feat(subscriptions): implement Subscription API and routing for subscription management

--- src/suscriptions/presentation/subscriptions-routes.js
+++
@@ -1,8 +0,0 @@
-const subscriptionsRoutes = [
-    {
-        path: "payments",
-        name: "payments",
-        component: () => import("./views/payments.vue"),
-    }
-    ];
-export default subscriptionsRoutes;
